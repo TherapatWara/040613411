3d039b65b02ecaf9305c6e4c2f44764abfec6a7b
Therapat2088 <118385264+Therapat2088@users.noreply.github.com>
2023-10-06 23:04:16 +0700

    qc

---
+++ project/login_emp.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="login-style.css">
+    </head>
+    <body>
+        <form action="login_emp_sql.php" method="get">
+        <div class="input-login">
+            <p>ID : </p>
+            <input type="text" name="emp_Id"><br>
+            <input type="submit" class="loginbut" value="LOGIN" name="login_user" >
+        </div>
+    </form>
+    </body>
+</html>
